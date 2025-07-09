cf55bb4331902004452916f89f74a49b8b2b09a2
Milan Yadav <ymilan593@gmail.com>
2025-07-09 23:14:37 +0545

    added auth test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,8 +1,7 @@
-from rest_framework import status
-from rest_framework.test import APITestCase
-
 from django.contrib.auth.models import User
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
 
@@ -17,3 +16,24 @@ class RegistrationTestCase(APITestCase):
         response = self.client.post(reverse('register'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(User.objects.filter(username="testuser").exists())
+
+class AuthTestCase(APITestCase):
+    def setUp(self):
+        self.username = 'authuser'
+        self.password = 'authpass123'
+        User.objects.create_user(username=self.username, password=self.password)
+
+    def test_login_success(self):
+        url = reverse('login')
+        data = {'username': self.username, 'password': self.password}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+    def test_login_failure(self):
+        url = reverse('login')
+        data = {'username': self.username, 'password': 'wrongpass'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotIn('access', response.data)
